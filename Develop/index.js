32b0eb46f2ddc6011d0f5f319a5c92b1a78a0d86
Will Darkins <willdarkins@gmail.com>
2021-09-30 15:39:33 -0700

    inquirer up and running

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
