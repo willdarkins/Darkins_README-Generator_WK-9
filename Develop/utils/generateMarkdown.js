44133368354dc67459aa9a318c721dc99728b509
Will Darkins <willdarkins@gmail.com>
2021-10-03 11:27:46 -0700

    badge issue resolved

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,21 +3,18 @@
 const renderLicenseBadge = license => {
   if (!license) {
     return '';
-  }
+  } 
   let badge = ''
-  if(license === 'MIT') {
+  if(license == 'MIT') {
     badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
-  } else if (license === 'APACHE 2.0') {
+  } else if (license == 'APACHE 2.0') {
     badge = `![APACHE 2.0](https://img.shields.io/badge/License-APACHE2.0-blue.svg)`
-  } else if (license === 'GPL') {
+  } else if (license == 'GPL') {
     badge = `![GPL license](https://img.shields.io/badge/License-GPL-brightgreen.svg)`
-  } else if (license === 'postgresql') {
+  } else if (license == 'postgresql') {
     badge = `![postgresql](https://img.shields.io/badge/License-postgresql-blueviolet.svg)`
   }
-   else {
-    badge = "N/A"
-  }
-  return license;
+  return badge;
 };
 
 // TODO: Create a function that returns the license link
@@ -30,11 +27,11 @@ const renderLicenseSection = license => {}
 
 // TODO: Create a function to generate markdown for README
 
-generateMarkdown = (data) => {
+generateMarkdown = data => {
   return `
 # ${data.projectName}
 
-${badge}
+${renderLicenseBadge(data.license)}
 
 * [Installation](#installation)
 * [Language](#language)
